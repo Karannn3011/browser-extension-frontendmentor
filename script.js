6b86e8ae1ec966b472e13271d1240c970ef6c5eb
Karannn3011 <karanchhipa30@gmail.com>
2025-05-16 19:27:25 +0530

    funcworking

--- script.js
+++ script.js
@@ -2,98 +2,140 @@ const container = document.querySelector(".container")
 const active = document.getElementById("active")
 const all = document.getElementById("all")
 const inactive = document.getElementById("inactive")
-let toggleBtns;
 
-console.log(document.querySelectorAll("input[name='sort'"))
 
 document.addEventListener("DOMContentLoaded", () => {
+    localStorage.clear();
+    let toggleBtns;
     fetch("data.json")
         .then((response) => response.json())
         .then((data) => {
             data.forEach((elem) => {
-                localStorage.setItem(`${elem.name}ext`, `${elem.isActive}`)
+                localStorage.setItem(elem.name.toLowerCase().replace(" ",""), JSON.stringify(elem));
             })
-            const allarr = data;
-            const activearr = data.filter((elem) => elem.isActive === true)
-            const inactivearr = data.filter((elem) => elem.isActive === false)
-            console.log(activearr)
-            console.log(inactivearr)
+
             console.log("DOM LOADED")
             all.addEventListener("change", () => {
                 container.innerHTML = ""
-                allarr.forEach(element => {
-                
-                container.innerHTML +=
-                    `<div id="${element.name.toLowerCase()}" class="w-[90%] bg-white mx-auto text-center p-4 rounded-xl my-5 shadow-md">
+                Object.keys(localStorage).forEach((key) => {
+                    let toAdd = JSON.parse(localStorage.getItem(key));
+                    container.innerHTML +=
+                        `<div id="${toAdd.name.toLowerCase().replace(" ", "")}extension" class="w-[90%] bg-white mx-auto text-center p-4 rounded-xl my-5 shadow-md">
                     <div class="flex items-start gap-x-4">
-                    <img class="" src="${element.logo}" alt="${element.name} logo">
+                    <img class="" src="${toAdd.logo}" alt="${key} logo">
                     <div class="text-left">
-                    <h1 class="text-xl font-bold mb-2">${element.name}</h1>
-                    <p class="text-neutral-600">${element.description}</p>
+                    <h1 class="text-xl font-bold mb-2">${toAdd.name}</h1>
+                    <p class="text-neutral-600">${toAdd.description}</p>
                     </div>
                     </div>
                     <div class="flex flex-row justify-between mt-4">
                     <button class="border-neutral-600 border-[1px] py-1 px-4 rounded-full" type="button">Remove</button>
                     <label class="relative flex items-center cursor-pointer">
-                        <input type="checkbox" id="${element.name.toLowerCase()}tog" class="toggleExt sr-only peer" ${element.isActive ? "checked" : ""}>
+                        <input type="checkbox" id="${toAdd.name.toLowerCase().replace(" ", "")}tog" class="toggleExt sr-only peer" ${toAdd.isActive ? "checked" : ""}>
                         <div class="w-9 h-5 bg-neutral-300 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[9px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-red-700 hover:peer-checked:bg-red-500"></div>
                     </label>
                     </div>
                 </div>`
+                })
+
                 toggleBtns = document.querySelectorAll(".toggleExt")
-            });
+                addTogging(toggleBtns);
+
             })
-            all.dispatchEvent(new Event("change"));
+            const tempev = new Event("change")
+            all.dispatchEvent(tempev);
+
+
+
+
 
             active.addEventListener("change", () => {
                 container.innerHTML = ""
-                activearr.forEach((element) => {
+                Object.keys(localStorage).forEach((key) => {
+                    let toAdd = JSON.parse(localStorage.getItem(key));
+                    if (toAdd.isActive) {
                         container.innerHTML +=
-                        `<div id="${element.name.toLowerCase()}" class="w-[90%] bg-white mx-auto text-center p-4 rounded-xl my-5 shadow-md">
+                            `<div id="${toAdd.name.toLowerCase().replace(" ", "")}extension" class="w-[90%] bg-white mx-auto text-center p-4 rounded-xl my-5 shadow-md">
                     <div class="flex items-start gap-x-4">
-                    <img class="" src="${element.logo}" alt="${element.name} logo">
+                    <img class="" src="${toAdd.logo}" alt="${key} logo">
                     <div class="text-left">
-                    <h1 class="text-xl font-bold mb-2">${element.name}</h1>
-                    <p class="text-neutral-600">${element.description}</p>
+                    <h1 class="text-xl font-bold mb-2">${toAdd.name}</h1>
+                    <p class="text-neutral-600">${toAdd.description}</p>
                     </div>
                     </div>
                     <div class="flex flex-row justify-between mt-4">
                     <button class="border-neutral-600 border-[1px] py-1 px-4 rounded-full" type="button">Remove</button>
                     <label class="relative flex items-center cursor-pointer">
-                        <input type="checkbox" id="${element.name.toLowerCase()}tog" class="toggleExt sr-only peer" ${element.isActive ? "checked" : ""}>
+                        <input type="checkbox" id="${toAdd.name.toLowerCase().replace(" ", "")}tog" class="toggleExt sr-only peer" ${toAdd.isActive ? "checked" : ""}>
                         <div class="w-9 h-5 bg-neutral-300 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[9px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-red-700 hover:peer-checked:bg-red-500"></div>
                     </label>
                     </div>
                 </div>`
-                toggleBtns = document.querySelectorAll(".toggleExt")   
+                    }
+
                 })
+                toggleBtns = document.querySelectorAll(".toggleExt")
+                addTogging(toggleBtns);
             })
 
             inactive.addEventListener("change", () => {
                 container.innerHTML = ""
-                inactivearr.forEach((element) => {
+                Object.keys(localStorage).forEach((key) => {
+                    let toAdd = JSON.parse(localStorage.getItem(key));
+                    if (!toAdd.isActive) {
                         container.innerHTML +=
-                        `<div id="${element.name.toLowerCase()}" class="w-[90%] bg-white mx-auto text-center p-4 rounded-xl my-5 shadow-md">
+                            `<div id="${toAdd.name.toLowerCase().replace(" ", "")}extension" class="w-[90%] bg-white mx-auto text-center p-4 rounded-xl my-5 shadow-md">
                     <div class="flex items-start gap-x-4">
-                    <img class="" src="${element.logo}" alt="${element.name} logo">
+                    <img class="" src="${toAdd.logo}" alt="${key} logo">
                     <div class="text-left">
-                    <h1 class="text-xl font-bold mb-2">${element.name}</h1>
-                    <p class="text-neutral-600">${element.description}</p>
+                    <h1 class="text-xl font-bold mb-2">${toAdd.name}</h1>
+                    <p class="text-neutral-600">${toAdd.description}</p>
                     </div>
                     </div>
                     <div class="flex flex-row justify-between mt-4">
                     <button class="border-neutral-600 border-[1px] py-1 px-4 rounded-full" type="button">Remove</button>
                     <label class="relative flex items-center cursor-pointer">
-                        <input type="checkbox" id="${element.name.toLowerCase()}tog" class="toggleExt sr-only peer" ${element.isActive ? "checked" : ""}>
+                        <input type="checkbox" id="${toAdd.name.toLowerCase().replace(" ", "")}tog" class="toggleExt sr-only peer" ${toAdd.isActive ? "checked" : ""}>
                         <div class="w-9 h-5 bg-neutral-300 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[9px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-red-700 hover:peer-checked:bg-red-500"></div>
                     </label>
                     </div>
                 </div>`
+                    }
                 })
-                toggleBtns = document.querySelectorAll(".toggleExt")
+
+                const toggleBtns = document.querySelectorAll(".toggleExt")
+                addTogging(toggleBtns);
             })
 
-        
+        })
+
+})
+
+function addTogging(toggleBtns) {
+    toggleBtns.forEach((btn) => {
+        btn.addEventListener("change", () => {
+            const btnid = btn.id.replace("tog", "")
+            const divToRemove = document.querySelector(`#${btnid}extension`)
+            if (active.checked) {
+                setTimeout(() => {
+                    divToRemove.remove()
+                }, 500)
+                let tempitem = JSON.parse(localStorage.getItem(btnid))
+                tempitem.isActive = !tempitem.isActive;
+                localStorage.setItem(btnid, JSON.stringify(tempitem))
+            } else if (inactive.checked) {
+                setTimeout(() => {
+                    divToRemove.remove()
+                }, 500)
+                let tempitem = JSON.parse(localStorage.getItem(btnid))
+                tempitem.isActive = !tempitem.isActive;
+                localStorage.setItem(btnid, JSON.stringify(tempitem))
+            } else if (all.checked) {
+                let tempitem = JSON.parse(localStorage.getItem(btnid))
+                tempitem.isActive = !tempitem.isActive;
+                localStorage.setItem(btnid, JSON.stringify(tempitem))
+            }
 
         })
-})
+    })
+}
